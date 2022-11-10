7324bfebddb140102b804fe0f308e12dc08924f4
Uchit N M <89153405+uchitnm@users.noreply.github.com>
2022-11-10 20:08:55 +0530

    Update makeSh.sh

--- day6/makeSh.sh
+++ day6/makeSh.sh
@@ -4,7 +4,7 @@ if [ ${#} != 0 ]
 then
     ( touch ${1} )
 
-    ( echo -e '#!/bin/bash \n #Uchit N M' > ${1} )
+    ( echo -e '#!/bin/bash\n#Uchit N M' > ${1} )
     exit
 else
     echo "Wrong Syntax : makeSh.sh <File Name"
